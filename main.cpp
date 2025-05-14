46ca54ac878a57170f23327b70fda5f9d245b507
canming huang <huangcmzzk@gmail.com>
2025-05-14 11:46:48 +0000

    support cli argument
    Signed-off-by: canming huang <huangcmzzk@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include "streamer_encoder_wrapper.h"
+#include <unistd.h>
 
-int main() {
-    streamer_encoder_wrapper enc((char *)"127.0.0.1", 5600);
+int main(int argc, char *argv[]) {
+    int port = 5600;
+    int opt;
+    while ((opt = getopt(argc, argv, "p:")) != -1) {
+        switch (opt) {
+            case 'p':
+                port = strtol(optarg,NULL,0);
+                break;
+            default:
+                printf("unknown arguments.\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    char *target_ip = argv[optind];
+
+    printf("target ip:%s port:%d\n",target_ip,port);
+    streamer_encoder_wrapper enc(target_ip, port);
     enc.run();
     return 0;
 }
